39935b4672bf5753b60640da8575f2ef4fa9192f
chipanatica <josepaulct@gmail.com>
2023-05-08 10:20:48 +0200

    AWS Architecture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -289,8 +289,8 @@ export default function Home() {
       <section className='bg-white dark:bg-gray-900'>
         <div
           className='gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6'>
-          <img className='w-full hidden dark:block'
-               src='https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg'
+          <img className='w-full hidden dark:block rounded rounded-lg'
+               src='images/takenote-arch.png'
                alt='dashboard image' />
           <div className='mt-4 md:mt-0'>
             <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white'>AWS Workflow</h2>
